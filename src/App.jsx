aee58f0b7525cf7dbfbc2ddb0b0a45787ee28f44
Tobiloba Orekoya <tobilobaorekoya19@gmail.com>
2022-11-20 11:56:28 +0100

    TEA 71 copy of my dev branch

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LandingPage from './pages/LandingPage';
+import Userupload from './pages/UserUpload';
+
+const App = () => (
+	<div className="App">
+		<LandingPage />
+		<Userupload />
+	</div>
+);
+export default App;
